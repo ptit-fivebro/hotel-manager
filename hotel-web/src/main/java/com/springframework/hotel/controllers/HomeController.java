5dcf44bf36cae1541e8a3ad336d323bc142ae327
thongho98 <hoquocthong456@gmail.com>
2020-10-23 00:55:26 +0700

    first commit

---
+++ hotel-web/src/main/java/com/springframework/hotel/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.springframework.hotel.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+        return "index";
+    }
+}
